73c1a466d66e09ead8aa7a48df10bb8b7c43cb79
hamidba <balalzadehhamid79@gmail.com>
2022-09-05 15:10:01 +0430

    register city and admin in admin

--- province/admin.py
+++ province/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from province.models import (Province,City)
 # Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name','province']
+    list_display_links = ['name']
+    list_filter = ['province']
+
+admin.site.register(Province)
+admin.site.register(City,CityAdmin)
--- province/models.py
+++ province/models.py
@@ -8,8 +8,14 @@ class Province(models.Model):
     name = models.CharField(max_length=50,blank=False,null=False)
     slug = models.SlugField(max_length=125,blank=False,null=False)
 
+    def __str__(self) :
+        return self.name
+        
 class City(models.Model):
     """City Model"""
     name = models.CharField(max_length=85,blank=False,null=False)
     slug = models.SlugField(max_length=225,blank=False,null=False)
     province = models.ForeignKey(Province, null=False, blank=False,on_delete=models.CASCADE)
+
+    def __str__(self) :
+        return self.name
