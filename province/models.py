5ed91ab328d437036c2ff98ed6a9d763138d114b
hamidba <balalzadehhamid79@gmail.com>
2022-09-05 14:22:45 +0430

    update province and city models

--- province/models.py
+++ province/models.py
@@ -6,8 +6,10 @@ from django.db import models
 class Province(models.Model):
     """Province Model"""
     name = models.CharField(max_length=50,blank=False,null=False)
+    slug = models.SlugField(max_length=125,blank=False,null=False)
 
 class City(models.Model):
     """City Model"""
     name = models.CharField(max_length=85,blank=False,null=False)
+    slug = models.SlugField(max_length=225,blank=False,null=False)
     province = models.ForeignKey(Province, null=False, blank=False,on_delete=models.CASCADE)
--- province/tests/tests_model.py
+++ province/tests/tests_model.py
@@ -4,9 +4,11 @@ Test Province Module
 from django.test import TestCase
 from province.models import (Province , City)
 
-def create_province(name):
+from django.template.defaultfilters import slugify
+
+def create_province(name,slug):
     """Helper Function To Create Province"""
-    return Province.objects.create(name=name)
+    return Province.objects.create(name=name,slug=slug)
 
 class ProvinceTest(TestCase):
     """Test Province Model"""
@@ -14,7 +16,8 @@ class ProvinceTest(TestCase):
     def test_create_province_should_work_propely(self):
         """Test To Create Province Object"""
         name = 'tehran'
-        province = Province.objects.create(name=name)
+        slug = slugify(name)
+        province = Province.objects.create(name=name,slug=slug)
         self.assertEqual(province.name , name)
 
 class CityTest(TestCase):
@@ -22,10 +25,10 @@ class CityTest(TestCase):
 
     def test_create_city_should_work_propely(self):
         """Test To Create City Object"""
-        province = create_province(name='sistan va baluchestan')
+        province = create_province(name='sistan va baluchestan',slug=slugify('sistan va baluchestan'))
         city_name = 'zahedan'
-
-        city = City.objects.create(name=city_name,province=province)
+        slug = slugify(city_name)
+        city = City.objects.create(name=city_name,slug=slug,province=province)
 
         self.assertEqual(city.name, city_name)
         self.assertEqual(city.province.name , province.name)
