d607645c70e7921a86fa607307a5e74150f0e085
hamidba <balalzadehhamid79@gmail.com>
2023-12-07 02:43:09 +0330

    delete category image after modification

--- cafe/signals.py
+++ cafe/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 
-from cafe.models import Cafe, Gallery
+from cafe.models import Cafe, Gallery, Category
 from notifications import KavenegarSMS
 
 
@@ -27,6 +27,31 @@ def post_save_image(sender, instance, *args, **kwargs):
 
 
 @receiver(pre_save, sender=Gallery)
+def pre_save_image(sender, instance, *args, **kwargs):
+    """instance old image file will delete from os"""
+    try:
+        old_img = instance.__class__.objects.get(id=instance.id).image.path
+        try:
+            new_img = instance.image.path
+        except:
+            new_img = None
+        if new_img != old_img:
+            import os
+
+            if os.path.exists(old_img):
+                os.remove(old_img)
+    except:
+        pass
+    
+@receiver(post_delete, sender=Category)
+def post_delete_category_image(sender, instance, *args, **kwargs):
+    """Clean Old Image file"""
+    try:
+        instance.image.delete(save=False)
+    except:
+        pass
+    
+@receiver(pre_save, sender=Category)
 def pre_save_image(sender, instance, *args, **kwargs):
     """instance old image file will delete from os"""
     try:
