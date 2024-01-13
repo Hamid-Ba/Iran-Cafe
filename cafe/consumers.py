38dcc8b0da36f9fc561020ba253489f59492f2fb
hamidba <balalzadehhamid79@gmail.com>
2024-01-13 17:58:48 +0330

    make pager WS broadcast with cafe scope

--- cafe/consumers.py
+++ cafe/consumers.py
@@ -53,40 +53,54 @@ class OrderConsumer(AsyncWebsocketConsumer):
 
 class PagerConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.group_name = "pager_group"
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        cafe_id = self.scope['url_route']['kwargs']['cafe_id']
+        self.cafe_group_name = f"cafe_{cafe_id}"
+        
+        await self.channel_layer.group_add(
+            self.cafe_group_name,
+            self.channel_name
+        )
+
         await self.accept()
 
-    async def disconnect(self, code):
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+    async def disconnect(self, close_code):
+            await self.channel_layer.group_discard(
+                self.cafe_group_name,
+                self.channel_name
+            )
 
     async def receive(self, text_data=None, bytes_data=None):
         try:
             text_data_json = json.loads(text_data)
             try:
                 cafe = text_data_json["cafe"]
+                table = text_data_json["table"]
             except:
                 await self.send(text_data=json.dumps({"message": "invalid data"}))
                 return
-            # Send the cafe message to all clients in the group, excluding the sender
+            
+            # Broadcast the message to all channels in the group
             await self.channel_layer.group_send(
-                self.group_name,
+                self.cafe_group_name,
                 {
                     "type": "pager_message",
                     "cafe": cafe,
-                    "client": self.channel_name,
-                },
+                    "table": table,
+                    "client": self.channel_name
+                }
             )
+            
         except json.JSONDecodeError:
             await self.send(text_data=json.dumps({"message": "invalid data"}))
 
     async def pager_message(self, event):
         cafe = event["cafe"]
+        table = event["table"]
         sender_name = event["client"]
 
         # Send the cafe message to the current client
         if self.channel_name != sender_name:
-            await self.send(text_data=json.dumps({"cafe": cafe}))
+            await self.send(text_data=json.dumps({"cafe": cafe, "table":table}))
 
         if self.channel_name == sender_name:
             await self.send(text_data=json.dumps({"message": "success"}))
--- cafe/routing.py
+++ cafe/routing.py
@@ -5,5 +5,5 @@ from cafe.consumers import OrderConsumer, PagerConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/order/", OrderConsumer.as_asgi()),
-    re_path(r"ws/pager/", PagerConsumer.as_asgi()),
+    re_path(r"ws/pager/(?P<cafe_id>\d+)/$", PagerConsumer.as_asgi()),
 ]
